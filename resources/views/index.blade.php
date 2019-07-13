2d145916197673b7474b18f669ba7b2df5633cd1
Ruben Garcia <rdgp99@gmail.com>
2019-07-13 06:47:44 -0300

    nothing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" async href="css/style.min.css">
-    <link rel="stylesheet" async href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Rubén García - Full Stack Developer</title>
     <style>
            
@@ -25,7 +25,13 @@
 			padding: 0;
             box-sizing:border-box;
             font-weight:500;
-		}
+        }
+        
+        @media only screen and(min-width:500px){
+            .responsive{
+                display:none;
+            }
+        }
     </style>
 </head>
 <body>
