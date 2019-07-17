6f3a126c0b2c508605429148e9003efee33852f2
Ruben Garcia <rdgp99@gmail.com>
2019-07-17 06:51:30 -0300

    fixing margins on presentation card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" async href="css/style.min.css">
+    <link rel="stylesheet" async href="css/style.css">
     <link rel="stylesheet" async href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Rubén García - Full Stack Developer</title>
     <style>
