4e4b048b3b4c877f6b3a15f2aa1e254f94d51ef3
Ruben Garcia <51353436+AnotherHater@users.noreply.github.com>
2019-08-11 20:41:13 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -420,7 +420,7 @@
         </div>
       </div>
     </footer>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" ></script>
+<script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" ></script>
 <script async src="https://cdn.jsdelivr.net/npm/sweetalert2@8" ></script>
 <script src="js/js.min.js"></script>
 </body>
